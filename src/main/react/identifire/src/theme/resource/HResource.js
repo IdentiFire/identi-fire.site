af287c8dd9ca3e1814d795247d7f63a1a5e36f1a
amir-schnabel <amir.schnabel.work@outlook.com>
2022-04-10 16:40:12 +0300

    Add file to new repo

---
+++ src/main/react/identifire/src/theme/resource/HResource.js
@@ -0,0 +1,8 @@
+import appbackground from '../images/background.png';
+import logoNtext from '../images/logoNtext.png';
+
+
+export const IMAGES = {
+    "appbackground": appbackground,
+	"logoNtext": logoNtext
+}
